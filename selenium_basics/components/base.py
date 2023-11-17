3e96c4902c5e3e034969846eb88d6deddb04aa12
sergeisrhaikin <shaikins@yahoo.com>
2023-11-17 16:16:48 +0100

    Python - Classes

--- selenium_basics/components/base.py
+++ selenium_basics/components/base.py
@@ -1,1 +1,15 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
+
+class Base:
+    BASE_VAR = "Base Var"
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click(self, locator):
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(locator)
+        )
+        element.click()
--- selenium_basics/components/filter.py
+++ selenium_basics/components/filter.py
@@ -1,1 +1,14 @@
+from .base import Base
 
+
+class LeftFilter(Base):
+    LOCATOR = "//*"
+
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    def select_option(self, option, visible=False):
+        print(self.BASE_VAR)
+        print(self.LOCATOR)
+        print(option)
+        print(visible)
